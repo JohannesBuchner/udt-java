2f6e995b893e8e64c19a308c1bf551a4bc76ffc4
bschuller <bschuller@bee50969-7b09-4b63-a8b0-f2441a869d12>
2010-04-15 16:41:17 +0000

    more cc
    git-svn-id: svn://svn.code.sf.net/p/udt-java/code@14 bee50969-7b09-4b63-a8b0-f2441a869d12

--- udt-java/trunk/src/main/java/udt/UDTCongestionControl.java
+++ udt-java/trunk/src/main/java/udt/UDTCongestionControl.java
@@ -1,62 +1,62 @@
 package udt;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 import udt.util.Util;
 
 public class UDTCongestionControl {
 
-	private static final Logger logger=Logger.getLogger(ClientSession.class.getName());
-	//The UDT entity that this congestion algorithm is bound
 	private final UDTSession session;
+
+	//round trip time in microseconds
 	private long roundTripTime;
-
+	
+	//rate in packets per second
 	private long packetArrivalRate=100;
+	
+	//link capacity in packets per second
 	private long estimatedLinkCapacity;
+	
 	long maxControlWindowSize=128;
-	long maxSegmentSize=32768;
-
-	// Periodical timer to send an ACK, in milliseconds
-	private long aCKPeriod; 
-	// How many packets to send one ACK, in packets
-	private long aCKInterval;
-	// if the RTO value is defined by users
-	private boolean userDefinedRTO; 
-	// Retransmission time out(RTO) value, microseconds
-	private long retransmissionTimeOut;                 
 
 	// Packet sending period = packetsendInterval, in microseconds
-	private double packetSendingPeriod=0.001;              
+	private double packetSendingPeriod=1;              
 	// Congestion window size, in packets
 	private long congestionWindowSize=16;
-	// estimated bandwidth, packets per second
-	private long estimatedBandwidth;	
-	//number of packets to be increase
+	
+	//number of packets to be increased in the next SYN period
 	private double numOfIncreasingPacket;		
-	//last rate increase time
-	long lastRateIncreaseTime;
+	
+	//last rate increase time (microsecond value)
+	long lastRateIncreaseTime=Util.getCurrentTime();
+	
 	/*if in slow start phase*/
 	boolean slowStartPhase=true;
 	
 	/*last ACKed seq no*/
 	long lastAckSeqNumber=-1;
+	
 	/*max packet seq. no. sent out when last decrease happened*/
 	private	long lastDecreaseSeqNo;
 
 	//value of packetSendPeriod when last decrease happened
 	long lastDecreasePeriod;
+	
 	//NAK counter
-	long nACKCount=0;
+	long nACKCount=1;
+	
 	//number of decreases in a congestion epoch
-	long congestionEpochDecreaseCount;
+	long congestionEpochDecreaseCount=1;
+	
 	//random threshold on decrease by number of loss events
 	long decreaseRandom; 
+	
 	//average number of NAKs per congestion
 	long averageNACKNum;
 
 	public UDTCongestionControl(UDTSession session){
 		this.session=session;
+		lastDecreaseSeqNo= session.getInitialSequenceNumber()-1;
 		init();
 	}
 
@@ -65,16 +65,7 @@ public class UDTCongestionControl {
 	 * when the UDT socket is conected 
 	 */
 	public void init() {
-		lastRateIncreaseTime= Util.getCurrentTime();//???
-		slowStartPhase = true;
-		congestionWindowSize = 16;
-		packetSendingPeriod=0;
-		decreaseRandom =1;
-		averageNACKNum=1;
-		nACKCount=1;
-		congestionEpochDecreaseCount=1;
-		//lastDecreasePeriod=1;//???
-		lastDecreaseSeqNo= session.getInitialSequenceNumber()-1;
+		
 	}
 
 	public void setRTT(long rtt, long rttVar){
@@ -93,6 +84,7 @@ public class UDTCongestionControl {
 	public double getSendInterval(){
 		return packetSendingPeriod ;
 	}
+	
 	/**
 	 * congestionWindowSize
 	 * @return
@@ -109,9 +101,10 @@ public class UDTCongestionControl {
 	public void onACK(long ackSeqno){
 		//the fixed size of a UDT packet 
 		long maxSegmentSize=UDPEndPoint.DATAGRAM_SIZE;
-		//1.if it is  in slow start phase,set the congestion window size the product
-		//of packet arrival rate and(rtt +SYN)
-		double A=packetArrivalRate*(roundTripTime+0.01);
+		
+		//1.if it is  in slow start phase,set the congestion window size 
+		//to the product of packet arrival rate and(rtt +SYN)
+		double A=packetArrivalRate*(roundTripTime/1e6+0.01);
 		if(slowStartPhase){
 			congestionWindowSize=16;
 			slowStartPhase=false;
@@ -128,16 +121,16 @@ public class UDTCongestionControl {
 			numOfIncreasingPacket=computeNumOfIncreasingPacket();
 		}
 		//4.update the send period :
-		packetSendingPeriod=(packetSendingPeriod*Util.getSYNTime())/
-					(packetSendingPeriod*numOfIncreasingPacket+Util.getSYNTime());
+		packetSendingPeriod=packetSendingPeriod*Util.getSYNTimeSeconds()/
+					(packetSendingPeriod*numOfIncreasingPacket+Util.getSYNTimeSeconds());
 	}
 
+	final double Beta=0.0000015/UDPEndPoint.DATAGRAM_SIZE;
 	public double computeNumOfIncreasingPacket (){
 		long B,C,S;
 		B=estimatedLinkCapacity;
 		C=packetArrivalRate;
-		S=maxSegmentSize;
-		double Beta=0.0000015/S;
+		S=UDPEndPoint.DATAGRAM_SIZE;
 		double logBase10=Math.log10( S*(B-C)*8 );
 		double power10 = Math.pow( 10.0,Math.ceil (logBase10) )* Beta;
 		double inc = Math.max(power10, 1/S);
@@ -156,7 +149,12 @@ public class UDTCongestionControl {
 		/*1) If it is in slow start phase, set inter-packet interval to 
       	   1/recvrate. Slow start ends. Stop. */
 		if(slowStartPhase){
-			packetSendingPeriod = 1/packetArrivalRate;
+			if(packetArrivalRate>0){
+				packetSendingPeriod = 1e6/packetArrivalRate;
+			}
+			else{
+				packetSendingPeriod=congestionWindowSize*(roundTripTime+Util.getSYNTime());
+			}
 			slowStartPhase = false;
 			return;
 		}
